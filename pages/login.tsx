ce5e262fd3f0c85f92495f48d861b7e97ab4160e
Kayo Souza <kayo.felipe.souza2014@gmail.com>
2023-01-29 12:07:30 -0300

    Improve login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,124 +1,109 @@
-import type { APILoginResponse } from "../typings/api"
-import type { Optional } from "../typings"
-import { Component, createRef } from "react"
+import { memo, useRef, useState } from "react"
 import { toast } from "react-toastify"
+import Router from "next/router"
 import Image from "next/image"
 import style from "../styles/modules/login.module.scss"
 
-interface LoginPageProps {
-	fetching: boolean
-	showSpinner: boolean
-}
+const ApiErrors = {
+	400: "Este formato não é válido.",
+	401: "Usuário ou senha estão incorretos.",
+	406: "Not Acceptable",
+	411: "As informações que você está tentando enviar são muito grande."
+} as const
 
-export default class LoginPage extends Component<{}, LoginPageProps> {
-	state: Readonly<LoginPageProps> = {
-		fetching: false,
-		showSpinner: false
-	}
+const Logo = memo(function Logo(){
+	return <>
+		<div className={style.image}>
+			<Image src="/images/logo.svg" alt="Logotipo do site" loading="eager" priority={true} fill />
+		</div>
+		<span>Flash</span>
+	</>
+})
 
-	ApiErrors = {
-		400: "Este formato não é válido.",
-		401: "Usuário ou senha estão incorretos.",
-		406: "Not Acceptable",
-		411: "As informações que você está tentando enviar são muito grande."
-	} as const
+export default function LoginPage(){
+	const usernameRef = useRef<HTMLInputElement>(null)
+	const passwordRef = useRef<HTMLInputElement>(null)
+	const submitRef = useRef<HTMLButtonElement>(null)
+	const [fetching, setFetching] = useState(false)
+	const [spinner, setSpinner] = useState(false)
 
-	addState(entries: Optional<LoginPageProps>): void
-	addState<T extends keyof LoginPageProps>(key: T, value: LoginPageProps[T]): void
-	addState<T extends keyof LoginPageProps>(arg: T | Optional<LoginPageProps>, value?: LoginPageProps[T]){
-		if(typeof arg === "string"){
-			return this.setState(prevState => ({ ...prevState, [arg]: value }))
-		}
+	return (
+		<main className={style.main}>
+			<section>
+				<Logo />
+			</section>
+			<section>
+				<form method="post" action="/api/login" name="login" onSubmit={async event => {
+					event.preventDefault()
 
-		this.setState(prevState => ({ ...prevState, ...arg }))
-	}
+					try{
+						if(fetching) throw "Já existe um envio sendo feito"
 
-	async Submit(username: string, password: string): Promise<APILoginResponse>{
-		const SetState = (value: boolean) => this.addState({ fetching: value, showSpinner: value })
-		const Enable = () => SetState(true)
-		const Disable = () => SetState(false)
-
-		Enable()
-
-		try{
-			const response = await fetch("/api/login", {
-				headers: {
-					"Accept": "text/html,*/*",
-					"Content-Type": "application/x-www-form-urlencoded"
-				},
-				body: new URLSearchParams({ username, password }),
-				method: "POST"
-			})
-
-			if(!response.ok) throw response.status
-
-			let data: APILoginResponse
-
-			try{
-				data = await response.json()
-			}finally{
-				Disable()
-			}
-
-			return data
-		}finally{
-			Disable()
-		}
-	}
-
-	render(){
-		const usernameRef = createRef<HTMLInputElement>()
-		const passwordRef = createRef<HTMLInputElement>()
-
-		return (
-			<main className={style.main}>
-				<section>
-					<div className={style.image}>
-						<Image src="/images/logo.svg" alt="Logotipo do site" loading="eager" priority={true} fill />
-					</div>
-					<span>Flash</span>
-				</section>
-				<section>
-					<form method="post" action="/api/login" name="login" onSubmit={async event => {
-						event.preventDefault()
+						setFetching(true)
+						setSpinner(true)
 
 						try{
-							if(this.state.fetching) throw "Já existe um envio sendo feito"
+							const response = await fetch("/api/login", {
+								headers: {
+									"Accept": "text/html,*/*",
+									"Content-Type": "application/x-www-form-urlencoded"
+								},
+								body: new URLSearchParams({
+									username: usernameRef.current!.value,
+									password: passwordRef.current!.value
+								}),
+								method: "POST"
+							})
 
-							const { current: username } = usernameRef
-							const { current: password } = passwordRef
-							const { success } = await this.Submit(username!.value, password!.value)
+							if(!response.ok) throw response.status
 
-							if(!success) throw success
+							const data = await response.json()
 
-							location.pathname = "/"
-						}catch(error){
-							switch(typeof error){
-								case "string": return toast.error(error)
-								case "number":
-									if(error in this.ApiErrors) return toast.error(this.ApiErrors[error as keyof typeof this.ApiErrors])
-								break
-							}
+							if(!data.success) throw data.error ?? data.success
 
-							toast.error("Algo deu errado")
-							console.error(error)
+							Router.push("/")
+						}finally{
+							setFetching(false)
+							setSpinner(false)
 						}
-					}}>
-						<fieldset>
-							<label>
-								<span>Usuário</span>
-								<input type="text" className="no-outline" name="username" ref={usernameRef} />
-							</label>
-							<label>
-								<span>Senha</span>
-								<input type="password" className="no-outline" name="password" ref={passwordRef} />
-							</label>
-						</fieldset>
+					}catch(error){
+						switch(typeof error){
+							case "string": return toast.error(error)
+							case "number":
+								if(error in ApiErrors) return toast.error(ApiErrors[error as keyof typeof ApiErrors])
+							break
+						}
 
-						<button className={`no-outline ${style.submit}`} type="submit">Entrar</button>
-					</form>
-				</section>
-			</main>
-		)
-	}
+						toast.error("Algo deu errado")
+						console.error(error)
+					}
+				}}>
+					<fieldset>
+						<label>
+							<span>Usuário</span>
+							<input type="text" className="no-outline" name="username" ref={usernameRef} onKeyPress={event => {
+								if(event.key === "Enter"){
+									event.preventDefault()
+									passwordRef.current!.focus()
+								}
+							}} />
+						</label>
+						<label>
+							<span>Senha</span>
+							<input type="password" className="no-outline" name="password" ref={passwordRef} onKeyPress={event => {
+								if(event.key === "Enter"){
+									event.preventDefault()
+									submitRef.current!.click()
+								}
+							}} />
+						</label>
+					</fieldset>
+
+					<button className={`no-outline ${style.submit}`} type="submit" ref={submitRef}>
+						{spinner ? <div className={style.spinner}></div> : "Entrar"}
+					</button>
+				</form>
+			</section>
+		</main>
+	)
 }
