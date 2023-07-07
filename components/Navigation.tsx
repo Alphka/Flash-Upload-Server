d4642fb17e091eec843a4fb6928d0803c04ce499
Kayo Souza <kayo.felipe.souza2014@gmail.com>
2023-07-07 20:48:44 -0300

    Fix notifications

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,4 @@
-import type { MutableRefObject, MouseEvent as ReactMouseEvent } from "react"
+import type { MutableRefObject, MouseEvent as ReactMouseEvent, Dispatch, SetStateAction, Attributes } from "react"
 import type { INotificationData } from "../pages/api/notifications"
 import type { AccessTypes } from "../typings/database"
 import type { APIResponse } from "../typings/api"
@@ -16,7 +16,7 @@ interface GlobalProps {
 
 interface NavigationProps extends GlobalProps {}
 interface MobileProps extends GlobalProps {}
-interface SearchFormProps extends GlobalProps { icon?: boolean }
+interface SearchFormProps extends GlobalProps { icon?: true, shouldFocus?: true }
 
 const locales = {
 	siteLogo: "Logotipo do site",
@@ -28,34 +28,42 @@ const locales = {
 	search: "Pesquisar"
 }
 
-const iconClass = "icon material-symbols-outlined"
-const fillIconClass = "icon fill material-symbols-outlined"
-
-const SearchForm = memo<SearchFormProps>(function SearchForm({ icon, search }){
+const SearchForm = memo<SearchFormProps>(function SearchForm({ icon, search, shouldFocus }){
+	const [autoFocus, setAutoFocus] = useState<boolean | undefined>(undefined)
+	const inputRef = useRef<HTMLInputElement>(null)
 	const router = useRouter()
+
+	useEffect(() => {
+		if(shouldFocus && router.asPath === "/search" && router.query.q){
+			if(inputRef.current){
+				inputRef.current.focus()
+				setAutoFocus(true)
+			}
+		}
+	}, [router.asPath])
 
 	return (
 		<div id="search">
 			<input type="search" placeholder="Faça uma pesquisa..." defaultValue={search} onKeyPress={event => {
-				const input: HTMLInputElement = event.currentTarget
+				const input = event.currentTarget
 
 				if(event.key === "Enter"){
-					event.preventDefault()
-
 					const value = input.value.trim()
 
 					if(!value) return
 
-					router.push("/search?q=" + encodeURIComponent(value))
+					event.preventDefault()
+					router.push("/search?q=" + encodeURIComponent(value), "/search")
 				}
-			}} autoComplete="off" />
-			{icon && <span className={iconClass}>search</span>}
+			}} autoComplete="off" autoFocus={autoFocus} ref={inputRef} />
+			{icon && <span className="icon material-symbols-outlined">search</span>}
 		</div>
 	)
 })
 
 function MobileMenu({ userAccess }: MobileProps){
 	const [searchBarCollapsed, setSearchBarCollapsed] = useState(true)
+	const [closeNotifications, setCloseNotifications] = useState(() => () => {})
 	const [contentHidden, setContentHidden] = useState(true)
 	const refMenu = useRef<HTMLDivElement>(null)
 	const refMenuIcon = useRef<HTMLSpanElement>(null)
@@ -89,31 +97,34 @@ function MobileMenu({ userAccess }: MobileProps){
 	}
 
 	const handleContentHide = useCallback((event: ReactMouseEvent<HTMLDivElement>) => {
-		if(contentHidden && !searchBarCollapsed) setSearchBarCollapsed(true)
+		if(contentHidden){
+			if(!searchBarCollapsed) setSearchBarCollapsed(true)
+			if(refNotificationsOpen) closeNotifications()
+		}
+
 		return handleEventHide(setContentHidden, contentHidden, refMenuIcon)(event)
 	}, [contentHidden, searchBarCollapsed])
 
 	const handleSearchBarHide = useCallback((event: ReactMouseEvent<HTMLDivElement>) => {
 		if(searchBarCollapsed){
 			if(!contentHidden) setContentHidden(true)
+			if(refNotificationsOpen) closeNotifications()
 			refSearchBar.current!.querySelector("input")?.focus()
 		}
 
 		return handleEventHide(setSearchBarCollapsed, searchBarCollapsed, refSearchBar)(event)
 	}, [contentHidden, searchBarCollapsed])
 
-	const closePopups = useCallback(() => {
+	const closeIcons = useCallback(() => {
 		if(!contentHidden) setContentHidden(true)
 		if(!searchBarCollapsed) setSearchBarCollapsed(true)
 	}, [contentHidden, searchBarCollapsed])
 
-	// TODO: Close notifications when the other popups open
-
 	return (
 		<div className="icons mobile">
-			<span className={iconClass} aria-label={locales.search} onClick={handleSearchBarHide} ref={refSearchIcon}>search</span>
-			<Notifications openRef={refNotificationsOpen} mobile={true} handleOpen={closePopups} />
-			<span className={iconClass} aria-label={locales.menu} onClick={handleContentHide} ref={refMenuIcon}>menu</span>
+			<span className="icon material-symbols-outlined" aria-label={locales.search} onClick={handleSearchBarHide} ref={refSearchIcon}>search</span>
+			<Notifications {...{ openRef: refNotificationsOpen, mobile: true, setCloseCallback: setCloseNotifications, closeIcons }} />
+			<span className="icon material-symbols-outlined" aria-label={locales.menu} onClick={handleContentHide} ref={refMenuIcon}>menu</span>
 
 			<div className={searchBarCollapsed ? "search collapsed" : "search"} aria-hidden={searchBarCollapsed} ref={refSearchBar}>
 				<SearchForm {...{ userAccess }} />
@@ -121,19 +132,19 @@ function MobileMenu({ userAccess }: MobileProps){
 
 			<div className={contentHidden ? "content hidden" : "content"} aria-hidden={contentHidden} ref={refMenu}>
 				<Link href="/documents" prefetch={false} aria-label={locales.documents}>
-					<span className={iconClass}>description</span>
+					<span className="icon material-symbols-outlined">description</span>
 					{locales.documents}
 				</Link>
 
 				{userAccess === "all" && (
 					<Link href="/settings" prefetch={false} aria-label={locales.settings}>
-						<span className={iconClass}>settings</span>
+						<span className="icon material-symbols-outlined">settings</span>
 						{locales.settings}
 					</Link>
 				)}
 
 				<Link href="/login?logout" prefetch={false} aria-label={locales.logout}>
-					<span className={iconClass}>logout</span>
+					<span className="icon material-symbols-outlined">logout</span>
 					{locales.logout}
 				</Link>
 			</div>
@@ -152,13 +163,46 @@ const Logo = memo(function Logo(){
 	)
 })
 
+function DateUTC(isoDate: string){
+	const [year, month, day] = isoDate.substring(0, 10).split("-").map(Number) as [number, number, number]
+	return Date.UTC(year, month - 1, day)
+}
+
+type INotificationProps = Pick<INotificationData, "folder" | "filename" | "expiresAt"> & Attributes
+
+const Notification = memo<INotificationProps>(function Notification({ key, folder, filename, expiresAt }){
+	const url = `/documents/${folder.reduced?.toLowerCase() || folder.name.toLowerCase()}`
+	const message = (() => {
+		const todayUTC = DateUTC(new Date().toISOString())
+		const expireUTC = DateUTC(expiresAt)
+
+		let daysLeft = Math.floor((expireUTC - todayUTC) / 86400000) // 1000 * 60 * 60 * 24
+
+		if(daysLeft === 0) return "Expira hoje"
+
+		return `${daysLeft > 0 ? "Irá expirar em" : (daysLeft = Math.abs(daysLeft), "Expirou há")} ${daysLeft} ${daysLeft === 1 ? "dia" : "dias"}`
+	})()
+
+	console.log(key)
+
+	return (
+		<div className="notification" key={key}>
+			<p className="title">
+				<Link href={url} prefetch={false}>{filename}</Link>
+			</p>
+			<p className="content">{message}</p>
+		</div>
+	)
+})
+
 interface NotificationsProps {
 	mobile?: boolean
 	openRef?: MutableRefObject<boolean | null>
-	handleOpen?: () => void
+	closeIcons?: () => void
+	setCloseCallback?: Dispatch<SetStateAction<() => void>>
 }
 
-const Notifications = memo(function Notifications({ mobile, openRef, handleOpen }: NotificationsProps){
+function Notifications({ mobile, openRef, closeIcons, setCloseCallback }: NotificationsProps){
 	const { data, error } = useSWR("/api/notifications", async (url: string) => {
 		const response = await fetch(url, { cache: "no-cache" })
 		const json = await response.json() as APIResponse<INotificationData[]>
@@ -171,6 +215,10 @@ const Notifications = memo(function Notifications({ mobile, openRef, handleOpen 
 	const [notificationsUnread, setNotificationsUnread] = useState(false)
 	const [contentHidden, setContentHidden] = useState(true)
 	const refMenu = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		if(setCloseCallback) setCloseCallback(() => () => setContentHidden(true))
+	}, [setCloseCallback])
 
 	const markAsRead = useCallback(() => {
 		if(data?.length){
@@ -216,9 +264,51 @@ const Notifications = memo(function Notifications({ mobile, openRef, handleOpen 
 		setNotificationsUnread(unread)
 	}, [data])
 
+	const Content = useCallback(function Content(){
+		if(error) return <p className="error">Algo deu errado</p>
+
+		if(data){
+			if(data.length){
+				// TODO: Get this from config
+				const limit = 5
+				const shouldLimit = data.length > limit
+				const notifications = data
+					.map(({ expiresAt, ...data }) => ({
+						expiresAt: new Date(expiresAt).getTime(),
+						expiresAtString: expiresAt,
+						...data
+					}))
+					.sort((a, b) => b.expiresAt - a.expiresAt)
+					.slice(0, limit)
+					.map(({ hash, expiresAtString, ...data }) => (
+						<Notification key={hash} {...{
+							...data,
+							expiresAt: expiresAtString
+						}} />
+					))
+
+				if(shouldLimit){
+					const limited = data.length - limit
+					const message = `${limited} ${limited === 1 ? " notificação foi oculta" : "notificações foram ocultas"}`
+
+					return <>
+						{notifications}
+						<p className="more">{message}</p>
+					</>
+				}
+
+				return <>{notifications}</>
+			}
+
+			return <p>Não há notificações no momento</p>
+		}
+
+		return <p>Carregando...</p>
+	}, [data, error])
+
 	return (
 		<div className={mobile ? "notifications mobile" : "notifications"} ref={refMenu}>
-			<span className={notificationsUnread ? fillIconClass : iconClass} onClick={event => {
+			<span className={notificationsUnread ? "icon material-symbols-outlined fill" : "icon material-symbols-outlined"} onClick={event => {
 				event.nativeEvent.stopImmediatePropagation()
 				event.preventDefault()
 
@@ -232,11 +322,11 @@ const Notifications = memo(function Notifications({ mobile, openRef, handleOpen 
 					window.removeEventListener("click", CloseHandler)
 				}
 
-				if(openRef) openRef.current = !contentHidden
+				if(openRef) openRef.current = contentHidden
 
 				if(contentHidden){
 					markAsRead()
-					handleOpen?.()
+					closeIcons?.()
 					setContentHidden(false)
 					window.addEventListener("click", CloseHandler)
 				}else{
@@ -247,50 +337,18 @@ const Notifications = memo(function Notifications({ mobile, openRef, handleOpen 
 				{notificationsUnread ? "notifications_active" : "notifications"}
 			</span>
 
-			<div className={"content".concat(contentHidden ? " hidden" : "")} aria-hidden={contentHidden}>
+			<div className={contentHidden ? "content hidden" : "content"} aria-hidden={contentHidden}>
 				<p className="title">
 					{locales.notifications}
 				</p>
 				<hr />
-				{error ? (
-					<p className="error">Algo deu errado</p>
-				) : data ? data.length ? (data.map(({ hash, folder, filename, expiresAt }) => {
-					function DateUTC(isoDate: string){
-						const [year, month, day] = isoDate.substring(0, 10).split("-").map(Number) as [number, number, number]
-						return Date.UTC(year, month - 1, day)
-					}
-
-					const url = `/documents/${folder.reduced?.toLowerCase() || folder.name.toLowerCase()}`
-					const message = (() => {
-						const todayUTC = DateUTC(new Date().toISOString())
-						const expireUTC = DateUTC(expiresAt)
-
-						let daysLeft = Math.floor((expireUTC - todayUTC) / 86400000) // 1000 * 60 * 60 * 24
-
-						if(daysLeft === 0) return "Expira hoje"
-
-						return `${daysLeft > 0 ? "Irá expirar em" : (daysLeft = Math.abs(daysLeft), "Expirou há")} ${daysLeft} ${daysLeft === 1 ? "dia" : "dias"}`
-					})()
-
-					return (
-						<div className="notification" key={hash}>
-							<p className="title">
-								<Link href={url} prefetch={false}>{filename}</Link>
-							</p>
-							<p className="content">{message}</p>
-						</div>
-					)
-				})) : (
-					<p>Não há notificações no momento</p>
-				) : (
-					<p>Carregando...</p>
-				)}
+				<Content />
 			</div>
 		</div>
 	)
-})
+}
 
-const Navigation = memo(function Navigation({ userAccess, search }: NavigationProps){
+const Navigation = memo<NavigationProps>(function Navigation({ userAccess, search }){
 	return (
 		<nav>
 			<Logo />
@@ -299,23 +357,24 @@ const Navigation = memo(function Navigation({ userAccess, search }: NavigationPr
 				<SearchForm {...{
 					userAccess,
 					search,
-					icon: true
+					icon: true,
+					shouldFocus: true
 				}} />
 
 				<Notifications />
 
 				<Link href="/documents" aria-label={locales.documents}>
-					<span className={iconClass}>description</span>
+					<span className="icon material-symbols-outlined">description</span>
 				</Link>
 
 				{userAccess === "all" && (
 					<Link href="/settings" prefetch={false} aria-label={locales.settings}>
-						<span className={iconClass}>settings</span>
+						<span className="icon material-symbols-outlined">settings</span>
 					</Link>
 				)}
 
 				<Link href="/login?logout" prefetch={false} aria-label={locales.logout}>
-					<span className={iconClass}>logout</span>
+					<span className="icon material-symbols-outlined">logout</span>
 				</Link>
 			</div>
 
